04a68f6a55995b08d605574bbbdd98df3e7583a9
Adam <adamtwhite6@gmail.com>
2018-12-05 17:08:27 -0500

    started test for pencil to write to paper

--- pencil-durability-pillar/src/main/java/com/main/Pencil.java
+++ pencil-durability-pillar/src/main/java/com/main/Pencil.java
@@ -2,4 +2,10 @@ package com.main;
 
 public class Pencil {
 
+	public String writeToPaper(String textFromPencil) {
+		String sendToPaper = textFromPencil;
+		
+		return sendToPaper;
+	}
+
 }
--- pencil-durability-pillar/src/test/java/com/main/PencilTest.java
+++ pencil-durability-pillar/src/test/java/com/main/PencilTest.java
@@ -1,5 +1,20 @@
 package com.main;
 
-public class PencilTest {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+public class PencilTest {
+	Pencil pencil;
+	
+	@Before
+	public void setup() {
+		pencil = new Pencil();
+	}
+	
+	@Test
+	public void can_pencil_send_text_to_paper() {
+		Assert.assertEquals("test", pencil.writeToPaper("test"));
+	}
+	
 }
