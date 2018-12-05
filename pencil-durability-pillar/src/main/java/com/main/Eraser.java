56fe4dab066479216aca0835524c4b6bd340293a
Adam <adamtwhite6@gmail.com>
2018-12-05 18:45:25 -0500

    made test for eraser losing health as it erases

--- pencil-durability-pillar/src/main/java/com/main/Eraser.java
+++ pencil-durability-pillar/src/main/java/com/main/Eraser.java
@@ -1,14 +1,23 @@
 package com.main;
 
 public class Eraser {
+	private int currentEraserHealth;
+	
+	public Eraser() {
+		currentEraserHealth = 60;
+	}
 
 	public String eraseWord(String wordToErase) {
 		String erasedWord = "";
 		for (int i = 0; i < wordToErase.length(); i++) {
 			erasedWord += " ";
+			currentEraserHealth--;
 		}
 		
 		return erasedWord;
 	}
-
+	
+	public int getCurrentEraserHealth() {
+		return currentEraserHealth;
+	}
 }
--- pencil-durability-pillar/src/test/java/com/main/EraserTest.java
+++ pencil-durability-pillar/src/test/java/com/main/EraserTest.java
@@ -8,15 +8,23 @@ import org.junit.Test;
 public class EraserTest {
 	Eraser eraser;
 	String testString;
+	int eraserAtFullHealth;
 	
 	@Before
 	public void setup() {
 		eraser = new Eraser();
 		testString = "This is a test for testing purposes!";
+		eraserAtFullHealth = eraser.getCurrentEraserHealth();
 	}
 	
 	@Test
 	public void can_eraser_erase_words() {
 		Assert.assertEquals("    ", eraser.eraseWord("test"));
+	}
+	
+	@Test
+	public void does_eraser_lose_health_as_it_erases() {
+		eraser.eraseWord("test");
+		Assert.assertTrue(eraser.getCurrentEraserHealth() < eraserAtFullHealth);
 	}
 }
