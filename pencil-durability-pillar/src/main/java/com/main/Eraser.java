e3a06dbf03f1b3e16b503b419f26ce54c900a709
Adam <adamtwhite6@gmail.com>
2018-12-05 18:35:56 -0500

    tested erasererasing words

--- pencil-durability-pillar/src/main/java/com/main/Eraser.java
+++ pencil-durability-pillar/src/main/java/com/main/Eraser.java
@@ -2,4 +2,14 @@ package com.main;
 
 public class Eraser {
 
+	public String eraseWord(String wordToErase) {
+		String erasedWord = "";
+		char[] wordArray = wordToErase.toCharArray();
+		for (char c : wordArray) {
+			erasedWord += " ";
+		}
+		
+		return erasedWord;
+	}
+
 }
--- pencil-durability-pillar/src/test/java/com/main/EraserTest.java
+++ pencil-durability-pillar/src/test/java/com/main/EraserTest.java
@@ -1,5 +1,22 @@
 package com.main;
 
-public class EraserTest {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+
+public class EraserTest {
+	Eraser eraser;
+	String testString;
+	
+	@Before
+	public void setup() {
+		eraser = new Eraser();
+		testString = "This is a test for testing purposes!";
+	}
+	
+	@Test
+	public void can_eraser_erase_words() {
+		Assert.assertEquals("    ", eraser.eraseWord("test"));
+	}
 }
