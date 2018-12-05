d802d894b1e84613ade1cfd98d0b5c650385160f
Adam <adamtwhite6@gmail.com>
2018-12-05 17:24:03 -0500

    tested pencil losing health as it writes

--- pencil-durability-pillar/src/main/java/com/main/Pencil.java
+++ pencil-durability-pillar/src/main/java/com/main/Pencil.java
@@ -1,11 +1,31 @@
 package com.main;
 
 public class Pencil {
+	private int maxPencilHealth = 60;
+	private int currentPencilHealth;
+	
+	public Pencil() {
+		setCurrentPencilHealth();
+	}
 
 	public String writeToPaper(String textFromPencil) {
-		String sendToPaper = textFromPencil;
+		String sendToPaper = "";
 		
+		for (int i = 0; i < textFromPencil.length(); i++) {
+			sendToPaper += textFromPencil.charAt(i);
+			currentPencilHealth--;
+		}
+
 		return sendToPaper;
+	}
+
+	public int getCurrentPencilHealth() {
+		// TODO Auto-generated method stub
+		return currentPencilHealth;
+	}
+	
+	public void setCurrentPencilHealth() {
+		this.currentPencilHealth = maxPencilHealth;
 	}
 
 }
--- pencil-durability-pillar/src/test/java/com/main/PencilTest.java
+++ pencil-durability-pillar/src/test/java/com/main/PencilTest.java
@@ -6,15 +6,29 @@ import org.junit.Test;
 
 public class PencilTest {
 	Pencil pencil;
+	int pencilHealthAtFull;
 	
 	@Before
 	public void setup() {
 		pencil = new Pencil();
+		pencilHealthAtFull = pencil.getCurrentPencilHealth();
 	}
 	
 	@Test
 	public void can_pencil_send_text_to_paper() {
 		Assert.assertEquals("test", pencil.writeToPaper("test"));
+	}
+	
+	@Test
+	public void does_pencil_lose_health_as_it_writes() {
+		pencil.writeToPaper("test");
+		Assert.assertTrue(pencil.getCurrentPencilHealth() < pencilHealthAtFull);
+	}
+	
+	@Test
+	public void do_uppercase_letters_subtract_two_points_from_health() {
+		pencil.writeToPaper("T");
+		Assert.assertEquals(pencilHealthAtFull - 2, pencil.getCurrentPencilHealth());
 	}
 	
 }
