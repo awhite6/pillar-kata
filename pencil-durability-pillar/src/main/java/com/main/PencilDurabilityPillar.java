9dbb710b29ff35d7f3c83cfe4416fcb72d8f61cc
Adam <adamtwhite6@gmail.com>
2018-12-06 16:23:10 -0500

    started putting menu together

--- pencil-durability-pillar/src/main/java/com/main/PencilDurabilityPillar.java
+++ pencil-durability-pillar/src/main/java/com/main/PencilDurabilityPillar.java
@@ -1,8 +1,34 @@
 package com.main;
 
 public class PencilDurabilityPillar {
+	private static final String MAIN_MENU_WELCOME = "Welcom to the pencil durability kata!";
+	private static final String PROMPT_TO_SELECT_OPTION = "Please select an option!";
+	private static final String GET_NEW_PIECE_OF_PAPER = "Get a new piece of paper";
+	private static final String QUIT = "quit";
+	private static final String[] MAIN_MENU_OPTIONS = { GET_NEW_PIECE_OF_PAPER, QUIT };
+	
+	private Menu menu;
+	
+	public PencilDurabilityPillar(Menu menu) {
+		this.menu = menu;
+	}
+	
+	public void run() {
+		while (true) {
+			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
+			
+			if (choice.equals(GET_NEW_PIECE_OF_PAPER)) {
+				System.out.println("congrats");
+			} else if (choice.equals(QUIT)) {
+				break;
+			}
+		}
+	}
+	
 	
 	public static void main(String[] args) {
-
+		Menu menu = new Menu(System.in, System.out);
+		PencilDurabilityPillar cli = new PencilDurabilityPillar(menu);
+		cli.run();
 	}  
-}
+} 
--- pencil-durability-pillar/src/test/java/com/main/MenuTest.java
+++ pencil-durability-pillar/src/test/java/com/main/MenuTest.java
@@ -7,9 +7,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-
-
-
 public class MenuTest {
 	Menu menu;
 	private ByteArrayOutputStream output;
@@ -44,6 +41,26 @@ public class MenuTest {
 		Integer result = (Integer)menu.getChoiceFromOptions(options);
 		
 		Assert.assertEquals(expected, result);	  
+	}
+	
+	@Test
+	public void redisplays_menu_if_user_does_not_choose_valid_option() {
+		Object[] options = new Object[] {  "Larry", "Curly", "Moe" };
+		Menu menu = getMenuForTestingWithUserInput("4\n1\n");
+		
+		menu.getChoiceFromOptions(options);
+		
+		String menuDisplay = "\n"+
+				 			 "1) "+options[0].toString()+"\n" + 
+						     "2) "+options[1].toString()+"\n" +
+						     "3) "+options[2].toString()+"\n\n" +
+						     "Please choose an option >>> ";
+		
+		String expected = menuDisplay + 
+					      "\n*** 4 is not a valid option ***\n\n" +
+					      menuDisplay;
+		
+		Assert.assertEquals(expected, output.toString());
 	}
 	
 
